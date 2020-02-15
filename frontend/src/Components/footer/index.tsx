44d5f37a12af21b3aa8f3fff0b545c8bf91d5ddf
tomal00 <tomas.malec00@gmail.com>
2020-02-15 05:29:59 +0100

    add references

--- frontend/src/Components/footer/index.tsx
+++ frontend/src/Components/footer/index.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const FooterWrapper = styled.div`
     height: 50px;
@@ -9,14 +10,39 @@ const FooterWrapper = styled.div`
     font-size: 16px;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 0 20px;
     color: ${p => p.theme.colors.secondary.text};
 `
 
+const References = styled.div`
+    font-size: 22px;
+`
+
+const StyledAnchor = styled.a`
+    color: white;
+    padding: 3px;
+`
+
+const Icon = styled(FontAwesomeIcon)`
+    color: white;
+    transition: color 0.2s ease-out;
+
+    @media (hover: hover) and (pointer: fine) {
+        ${StyledAnchor}:hover & {
+            color: ${p => p.theme.colors.secondary.base};
+        }
+    }
+`
 
 export default () => {
 
     return <FooterWrapper>
-        2020, Created by Tomáš Malec
+        <span>2020, Created by Tomáš Malec</span>
+        <References>
+            <StyledAnchor target='_blank' href={encodeURI('https://github.com/tomal00/shara')} >
+                <Icon icon={['fab', 'github']} />
+            </StyledAnchor>
+        </References>
     </FooterWrapper>
 }
--- frontend/src/icons.ts
+++ frontend/src/icons.ts
@@ -2,9 +2,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import {
     faCopy, faChevronRight, faBars, faSyncAlt, faLock, faUnlock, faLockOpen, faChevronDown, faUser, faInfo, faCloudUploadAlt, faImages, faSearch, faPlusSquare, faTrashAlt
 } from '@fortawesome/free-solid-svg-icons'
+import {
+    faGithub
+} from '@fortawesome/free-brands-svg-icons'
 
 export default {
     init(): void {
-        library.add(faCopy, faChevronRight, faBars, faSyncAlt, faLock, faUnlock, faLockOpen, faChevronDown, faUser, faInfo, faCloudUploadAlt, faImages, faSearch, faPlusSquare, faTrashAlt)
+        library.add(faGithub, faCopy, faChevronRight, faBars, faSyncAlt, faLock, faUnlock, faLockOpen, faChevronDown, faUser, faInfo, faCloudUploadAlt, faImages, faSearch, faPlusSquare, faTrashAlt)
     }
 }
